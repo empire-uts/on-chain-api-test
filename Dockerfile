fb79eb426f7c2c0ec78ea6fd5c1f032d346c0d4a
EC2 Default User <ec2-user@ip-10-0-0-20.ap-northeast-1.compute.internal>
2023-02-28 16:26:07 +0000

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,24 @@
 ARG FUNCTION_DIR="/home/app/"
 ARG RUNTIME_VERSION="3.8"
-ARG DISTRO_VERSION="3.12"
 
-FROM python:${RUNTIME_VERSION}-alpine${DISTRO_VERSION} AS python-alpine
-RUN apk add --no-cache \
+FROM python:${RUNTIME_VERSION}-slim-bullseye AS python-alpine
+RUN apt-get install -o APT::Keep-Downloaded-Packages=false \
     libstdc++
 
 FROM python-alpine AS build-image
-RUN apk add --no-cache \
-    build-base \
-    libtool \
-    autoconf \
-    automake \
-    libexecinfo-dev \
+RUN apt-get update && \
+    apt-get install -y \
+    # apt-get install -o APT::Keep-Downloaded-Packages=false \
+    g++ \
     make \
     cmake \
-    libcurl
+    unzip \
+    libcurl4-openssl-dev
 ARG FUNCTION_DIR
 ARG RUNTIME_VERSION
 RUN mkdir -p ${FUNCTION_DIR}
 COPY . ${FUNCTION_DIR}
+RUN python${RUNTIME_VERSION} -m pip install --upgrade pip
 RUN python${RUNTIME_VERSION} -m pip install awslambdaric --target ${FUNCTION_DIR}
 
 FROM python-alpine
@@ -30,7 +29,12 @@ ADD https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest
 COPY entry.sh /
 COPY requirements.txt /
 RUN chmod 755 /usr/bin/aws-lambda-rie /entry.sh
-RUN apk add git
-RUN pip install -r requirements.txt
+RUN apt-get update && \
+    apt-get install -y git
+RUN python${RUNTIME_VERSION} -m pip install --upgrade pip
+RUN python${RUNTIME_VERSION} -m pip install numpy
+RUN python${RUNTIME_VERSION} -m pip install python-dateutil
+RUN python${RUNTIME_VERSION} -m pip install pytz
+RUN python${RUNTIME_VERSION} -m pip install -r requirements.txt
 ENTRYPOINT [ "/entry.sh" ]
 CMD [ "src/app.handler" ]
--- src/app.py
+++ src/app.py
@@ -1,38 +1,106 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from pydantic import BaseModel
+from typing import Union
+from datetime import datetime, timedelta, date
+import requests
+import json
+import time
+
+import boto3
+import pandas as pd
+
+headers = {
+    "accept": "application/json",
+    "X-API-KEY": "5aecfc59-6216-47cd-81f0-397093b41221"
+}
 
 app = FastAPI()
+cache = {}
+cash_time = 60
+
+def get_cache(key, func, expiration_seconds = 60):
+    import datetime
+    global cache
+    print(cache)
+    # キャッシュから取得
+    if key in cache:
+        now = datetime.datetime.now()
+        if (cache[key]["updated"] + datetime.timedelta(seconds=expiration_seconds)) > now:
+            print("cashやで")
+            a = 1
+            return cache[key]["value"], a
+
+    # キャッシュ更新
+    value = func()
+    now = datetime.datetime.now()
+    cache[key] = {
+        "updated" : now,
+        "value"   : value
+    }
+    a = 0
+    return value, a
+
+class Athena(object):
+    def __init__(self, session=None):
+        self.client = session.client('athena')
+
+    def execute_sql(self, sql):
+        athena_client = self.client
+
+        exec_id = athena_client.start_query_execution(
+            QueryString=sql,
+            QueryExecutionContext={"Database": 'astar'},
+            ResultConfiguration={
+                'OutputLocation': 's3://magpy-indexer-bucket-test/test'
+            },
+            WorkGroup='primary'
+        )['QueryExecutionId']
+
+        print('Athena Execution ID: {}'.format(exec_id))
+        print('query: ')
+        print(sql)
+
+        status = athena_client.get_query_execution(QueryExecutionId=exec_id)['QueryExecution']['Status']
+
+        # ポーリング
+        while status['State'] not in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
+            print('{}: wait query running...'.format(status['State']))
+            time.sleep(5)
+            status = athena_client.get_query_execution(QueryExecutionId=exec_id)['QueryExecution']['Status']
+
+        return status['State'], exec_id
 
 
-class HelloParam(BaseModel):
-    name: str
+def main(string: date0, string: date1):
+    session = boto3.session.Session()
+
+    athena = Athena(session)
+    
+    sql = 'select "token0_symbol", "token0_amount", "token1_symbol", "token1_amount" from "dex_trades" where "block_timestamp"<' + {date0} + ' and "block_timestamp">' + {date1} + ' order by "block_timestamp" asc limit 50;'
+
+    status, exec_id = athena.execute_sql(sql)
+
+    if status == 'SUCCEEDED':
+        s3_client = session.client('s3')
+        body = s3_client.get_object(Bucket='pyapi-practice',
+                                    Key='athena-output/{0}.csv'.format(exec_id))['Body']
+
+        df = pd.read_csv(body, lineterminator='n')
+
+        print('[result]')
+        print(df)
 
 
-@app.get("/hello")
-def get_hello(name: str = None):
-    """
-    getで返事する
-    """
-    if name:
-        message = f"[GET]hello, {name}!"
-    else:
-        message = f"[GET]hello, visitor!"
-
-    return {"message": message}
+@app.get("/root")
+def root():
+    return {"message": "Hello World"}
 
 
-@app.post("/hello_post")
-def post_hello(param: HelloParam):
-    """
-    postで返事する
-    """
-    if param.name:
-        message = f"[POST]hello, {param.name}!"
-    else:
-        message = f"[POST]hello, visitor!"
-
-    return {"message": message}
-
+@app.get("/dexTrades/{date0}/{date1}")
+def dexTrades(date0, date1):
+    f = f"main({date0}, {date1})"
+    response = get_cache(f, main(date0, date1), cash_time)[0]
+    return response
 
 handler = Mangum(app)
